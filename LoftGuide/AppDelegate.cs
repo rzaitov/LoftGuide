c5c633a030d79ae4536b72ee987638f23a2e8969
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2013-09-06 22:29:13 +0400

    сохранение проекта

---
+++ LoftGuide/AppDelegate.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace LoftGuide
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		private UIWindow window;
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+			window.MakeKeyAndVisible();
+
+			return true;
+		}
+	}
+}
+
