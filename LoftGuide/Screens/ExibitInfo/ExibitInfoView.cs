de72d79497c8620bda5ccb42ab215a1006f8ba94
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2013-09-06 23:03:56 +0400

    Добавил навбар с кнопкой назад. У кнопки пустой обработчик.

--- LoftGuide/Screens/ExibitInfo/ExibitInfoView.cs
+++ LoftGuide/Screens/ExibitInfo/ExibitInfoView.cs
@@ -8,9 +8,36 @@ namespace LoftGuide.ExibitInfoScreen
 {
 	public class ExibitInfoView : UIView
 	{
+		private UINavigationBar _navBar;
+		private static readonly RectangleF NavbarFrame = new RectangleF(0f, 0f, 320f, 45f);
+		private UINavigationItem _backNavItem;
+
 		public ExibitInfoView()
 		{
 			BackgroundColor = UIColor.White;
+
+			InitNavBar();
+
+			AddSubview(_navBar);
+		}
+
+		private void InitNavBar()
+		{
+			_navBar = new UINavigationBar();
+			_navBar.Frame = NavbarFrame;
+			_navBar.ClipsToBounds = true;
+
+			UIBarButtonItem _backBtnItem = new UIBarButtonItem("Сканировать еще", UIBarButtonItemStyle.Plain, OnBackPressed);
+
+			_backNavItem = new UINavigationItem();
+			_backNavItem.LeftBarButtonItem = _backBtnItem;
+
+			_navBar.SetItems(new UINavigationItem[] { _backNavItem }, false);
+		}
+
+		private void OnBackPressed(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
