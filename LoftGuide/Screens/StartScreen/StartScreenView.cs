e249e3fde26963759b4f7472aab474e7b343f1f2
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2013-09-08 02:32:52 +0400

    Добавил контроллер и соответствущую вьюху для стартового экрана. Они пока пустые.

---
+++ LoftGuide/Screens/StartScreen/StartScreenView.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.UIKit;
+
+namespace LoftGuide.Screens.StartScreen
+{
+	public class StartScreenView : UIView
+	{
+		public StartScreenView()
+		{
+		}
+	}
+}
+
---
+++ LoftGuide/Screens/StartScreen/StartScreenViewController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.UIKit;
+
+namespace LoftGuide.Screens.StartScreen
+{
+	public class StartScreenViewController : UIViewController
+	{
+		private StartScreenView _view;
+
+		public StartScreenViewController()
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+		}
+	}
+}
+
