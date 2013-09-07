c02f13fb694b982c7858ae6e40ce5b262d109d99
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2013-09-08 02:26:18 +0400

    Добавил в проект контроллер, который будет делать переключение экранов

---
+++ LoftGuide/Screens/MainViewController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace LoftGuide.Screens
+{
+	public class MainViewController : UIViewController
+	{
+		public MainViewController()
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+		}
+	}
+}
+
